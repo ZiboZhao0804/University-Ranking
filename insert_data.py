8fdebc542f8336e2831baa2dec7dfe7d003deaf1
ZiboZhao <zibo.zhao@northwestern.edu>
2021-10-27 16:04:25 -0500

    update insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -1,7 +1,9 @@
 #import dependencies
 import pandas as pd
-from pymongo import MongoClient
+from pymongo import MongoClient, InsertOne
 import json
+import os
+import certifi
 
 heroku = False
 if heroku:
@@ -14,16 +16,37 @@ def mongoimport(csv_path, db_name, coll_name):
     """ Imports a csv file at path csv_name to a mongo colection
     returns: count of the documants in the new collection
     """
-    client = MongoClient("mongodb+srv://UniversityRankings:{mongoKey}@cluster0.02mpl.mongodb.net/collegeDB?retryWrites=true&w=majority")
+   
+    client = MongoClient(f"mongodb+srv://UniversityRankings:{mongoKey}@cluster0.02mpl.mongodb.net/collegeDB?retryWrites=true&w=majority",tlsCAFile=certifi.where())
     db = client[db_name]
     coll = db[coll_name]
     data = pd.read_csv(csv_path)
     payload = json.loads(data.to_json(orient = 'records'))
     coll.remove()
     coll.insert(payload)
-    return coll.count()
+    # return coll.count()
+    client.close()
+
 
 mongoimport('Data/data.csv','collegeDB','colleges')
 mongoimport('Data/states.csv','collegeDB','states')
+
+# def mongoimportjson(json_path,db_name,coll_name):
+#     client = MongoClient("mongodb+srv://UniversityRankings:{mongoKey}@cluster0.02mpl.mongodb.net/collegeDB?retryWrites=true&w=majority")
+#     db = client[db_name]
+#     coll = db[coll_name]
+#     requesting = []
+
+#     with open(json_path) as f:
+#         for jsonObj in f:
+#             myDict = json.loads(jsonObj)
+#             requesting.append(InsertOne(myDict))
+
+#     coll = coll.bulk_write(requesting)
+#     client.close()
+
+# mongoimportjson('Data/fullStatesData.json','collegeDB','statesData')
+
+
 
 
