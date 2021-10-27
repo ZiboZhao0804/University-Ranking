61d6ecd1758c43675bb72945f90e2cb4edf76206
ZiboZhao <zibo.zhao@northwestern.edu>
2021-10-27 16:28:31 -0500

    update insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -24,28 +24,25 @@ def mongoimport(csv_path, db_name, coll_name):
     payload = json.loads(data.to_json(orient = 'records'))
     coll.remove()
     coll.insert(payload)
-    # return coll.count()
-    client.close()
+    return coll.count()
 
 
 mongoimport('Data/data.csv','collegeDB','colleges')
 mongoimport('Data/states.csv','collegeDB','states')
 
-# def mongoimportjson(json_path,db_name,coll_name):
-#     client = MongoClient("mongodb+srv://UniversityRankings:{mongoKey}@cluster0.02mpl.mongodb.net/collegeDB?retryWrites=true&w=majority")
-#     db = client[db_name]
-#     coll = db[coll_name]
-#     requesting = []
+def mongoimportjson(json_path,db_name,coll_name):
+    client = MongoClient(f"mongodb+srv://UniversityRankings:{mongoKey}@cluster0.02mpl.mongodb.net/collegeDB?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+    db = client[db_name]
+    coll = db[coll_name]
+    requesting = []
+    with open(json_path) as f:
+        for jsonObj in f:
+            myDict = json.loads(jsonObj)
+            requesting.append(InsertOne(myDict))
+    coll = coll.bulk_write(requesting)
+    client.close()
 
-#     with open(json_path) as f:
-#         for jsonObj in f:
-#             myDict = json.loads(jsonObj)
-#             requesting.append(InsertOne(myDict))
-
-#     coll = coll.bulk_write(requesting)
-#     client.close()
-
-# mongoimportjson('Data/fullStatesData.json','collegeDB','statesData')
+mongoimportjson('Data/fullStatesData.json','collegeDB','statesData')
 
 
 
